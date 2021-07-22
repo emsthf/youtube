ee911ac1b070d609a8ad8a61fb171c8a15c55544
emsthf <deunsol916@gmail.com>
2021-07-22 11:26:26 +0900

    detail and back to list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
-import Videolist from './components/video_list/video_list';
+import VideoDetail from './components/video_detail/video_detail';
+import VideoList from './components/video_list/video_list';
 
 function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
   const search = query => {
+    setSelectedVideo(null);
     youtube.search(query).then(videos => setVideos(videos));
   };
 
@@ -15,7 +22,14 @@ function App({youtube}) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search}/>
-      <Videolist videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && <div className={styles.detail}>
+          <VideoDetail video={selectedVideo} />
+        </div>}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo? 'list' : 'grid'} />;
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = ({video: {snippet}}) => ( // props로 video의 snippet을 받아오고
-    <li className={styles.container}>
-        <div className={styles.video}>
-            <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
-            <div className={styles.metadata}>
-                <p className={styles.title}>{snippet.title}</p>
-                <p className={styles.channel}>{snippet.channelTitle}</p>
+const VideoItem = ({video, video: {snippet}, onVideoClick, display}) => { // props로 video와 video의 snippet, onVideoCilck, display를 받아오고
+    const displayType = display === 'list' ? styles.list : styles.grid;
+    return (
+        <li className={`${styles.container} ${displayType}`} onClick={() => onVideoClick(video)}>
+            <div className={styles.video}>
+                <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+                <div className={styles.metadata}>
+                    <p className={styles.title}>{snippet.title}</p>
+                    <p className={styles.channel}>{snippet.channelTitle}</p>
+                </div>
             </div>
-        </div>
-    </li>
-);
+        </li>
+    );
+};
 
 export default VideoItem;
